e73a4a355f369e4e4ce9539d95b36ef4460119c3
Stimul88 <Stimul881988@mail.ru>
2023-01-14 12:33:16 +0300

    Task 2

--- Home_work_classes.py
+++ Home_work_classes.py
@@ -6,6 +6,16 @@ class Student:
         self.finished_courses = []
         self.courses_in_progress = []
         self.grades = {}
+        self.courses_attached = []
+
+    def rate_1(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_attached and course in lector.courses_in_progress:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -13,7 +23,16 @@ class Mentor:
         self.name = name
         self.surname = surname
         self.courses_attached = []
+        self.grades = {}
+        self.finished_courses = []
+        self.courses_in_progress = []
 
+
+class Lecturer(Mentor):
+    grades = {}
+
+
+class Reviewer(Mentor):
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -23,19 +42,29 @@ class Mentor:
         else:
             return 'Ошибка'
 
-class Lecturer(Mentor):
-
-
-class Reviewer(Mentor):
-
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
+best_student.courses_attached += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
+cool_lector = Lecturer('Some', 'Buddy')
+cool_lector.courses_in_progress += ['Python']
+cool_lector.courses_attached += ['Python']
 
+cool_reviewer = Reviewer('Some', 'Buddy')
+cool_reviewer.courses_attached += ['Python']
+
+
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+cool_reviewer.rate_hw(best_student, 'Python', 10)
+
+best_student.rate_1(cool_lector, 'Python', 5)
+best_student.rate_1(cool_lector, 'Python', 8)
+best_student.rate_1(cool_lector, 'Python', 7)
 
 print(best_student.grades)
+print(cool_lector.grades)
